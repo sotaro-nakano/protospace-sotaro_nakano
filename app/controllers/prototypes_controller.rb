ef00669e6bfb369955bab1453db7d77ef916bfab
sotaro-nakano <sotaro_nakano@mail.ncn-ncc.co.jp>
2026-05-26 13:29:34 +0900

    プロトタイプ関連の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,63 @@
 class PrototypesController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+
   def index
+    @prototypes = Prototype.all
   end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    @prototype.user = current_user
+
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user == @prototype.user
+      redirect_to root_path
+    end
+  end
+
 end
