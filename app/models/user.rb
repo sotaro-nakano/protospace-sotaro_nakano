1a67119d6b2f50be5a19cc4eaf243d728991de08
sotaro-nakano <sotaro_nakano@mail.ncn-ncc.co.jp>
2026-05-28 10:28:12 +0900

    デプロイ後の修正１

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
 
   has_many :prototypes
   has_many :comments
+
+  validates :name, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
 end
